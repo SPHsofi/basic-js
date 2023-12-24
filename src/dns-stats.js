237014dc590ead64776431b5d2ed59c9eb80037a
SPHsofi <Sofia_pash@mail.ru>
2023-12-24 21:20:32 +0300

    feat: solved task (getDNSStats)

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsStats = {};
+  
+  for (const domain of domains) {
+    const domainParts = domain.split('.').reverse();
+    let currentDomainPart = '';
+
+    for (const part of domainParts) {
+      currentDomainPart += `.${part}`;
+
+      dnsStats[currentDomainPart] = (dnsStats[currentDomainPart] || 0) + 1;
+    }
+  }
+
+  return dnsStats;
 }
 
 module.exports = {
